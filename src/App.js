0fe1a7633e9a72a8a0972fa09830d9bfe58085e0
yatinsan <yatinksan@gmail.com>
2021-06-05 16:40:58 +0530

    todo with com and not comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { React, useState, } from 'react'
 import './App.css'
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
       </div>
       <div className="input">
         <input onChange={(e) => { setToDo(e.target.value) }} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => { setToDos([...toDos, { id: Date.now, text: toDo, status: false }]) }} className="fas fa-plus"></i>
+        <i onClick={() => { setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]) }} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {toDos.map((obj) => {
@@ -27,26 +27,66 @@ function App() {
               <div className="left">
                 <input value={obj.status}
                   onChange={(e) => {
-                    console.log(e.target.checked)
-                    
-                    setToDos(toDos.filter(obj2=>{
-                      if(obj2.id===obj.id){
-                       obj2.status=e.target.checked
+                    console.log(obj.id)
+                    setToDos(toDos.filter(obj2 => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked
                       }
                       return obj2
                     }))
-                    console.log(obj)
                   }} type="checkbox" name="" id="" />
                 <p>{obj.text}</p>
               </div>
               <div className="right">
-                <i onClick={()=>{setToDos(toDos.filter(obj3=>{
-                  
-                  return obj3.text !== obj.text
-                }))}} className="fas fa-times"></i>
+                <i onClick={() => {
+                  setToDos(toDos.filter(obj3 => {
+
+                    return obj3 !== obj
+                  }))
+                }} className="fas fa-times"></i>
               </div>
             </div>
           )
+        })}
+      </div>
+      <div className="didlist">
+       <h1>completed</h1>
+        {toDos.map((obj) => {
+          console.log(obj.text)
+          console.log(obj.status)
+          if (obj.status) {
+            return (
+              <div className="acc">
+              <div className="todo">
+                <div className="left">
+                  
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                </div>
+              </div>
+              </div>)
+          }
+          return null
+        })}
+         <h1>not completed</h1>
+        {toDos.map((obj) => {
+          console.log(obj.text)
+          console.log(obj.status)
+          if (!obj.status) {
+            return (
+              <div className="notacc">
+              <div className="todo">
+                <div className="left">
+                  
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                </div>
+              </div>
+              </div>)
+          }
+          return null
         })}
       </div>
     </div>
